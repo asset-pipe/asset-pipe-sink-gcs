8f1dd23b1e8d495b3737c2581a78880e8479c694
Trygve Lie <post@trygve-lie.com>
2016-11-21 15:34:15 +0100

    initial test setup

--- test/sink.js
+++ test/sink.js
@@ -1,37 +1,12 @@
 "use strict";
 
-const stream = require('stream');
-const concat = require('concat-stream');
-const Sink = require('../');
 const tap = require('tap');
+const Writer = require('../');
 
+tap.test('foo() - bar', function (t) {
+//    var writer = new Writer('./test/mock/main.js');
+//    writer.bundle().pipe(process.stdout);
 
-const sourceStream = (arr) => {
-    return new stream.Readable({
-        objectMode : false,
-        read: function (n) {
-            arr.forEach((chunk) => {
-                this.push(chunk);
-            });
-            this.push(null);
-        }
-    });
-}
-
-const a = ['a','b','c'];
-const b = ['d', 'a','b','c'];
-
-
-tap.test('not a real test', (t) => {
-    const sink = new Sink;
-    sourceStream(a).pipe(sink.writer('js', (name) => {
-        console.log(name);
-
-        sink.reader(name).pipe(concat((m) => {
-            console.log(m.toString());
-            t.end();
-        }));
-
-    }));
-
+    t.equal(true, true);
+    t.end();
 });
